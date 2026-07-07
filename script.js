c2cfd444ff065c6f29ea03efc9ddef867da4a56a
manthantyagi12-glitch <manthantyagi12@gmail.com>
2026-07-07 15:13:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const message =
+"I made this little website just to remind you how loved you are ❤️";
+
+let i = 0;
+
+function typeWriter(){
+
+if(i < message.length){
+
+document.getElementById("typing").innerHTML += message.charAt(i);
+
+i++;
+
+setTimeout(typeWriter,45);
+
+}
+
+}
+
+typeWriter();
+
+document.getElementById("openBtn").addEventListener("click",()=>{
+
+alert(
+"Welcome ❤️\n\nPart 2 will open your Comfort Box 🌸"
+);
+
+});
